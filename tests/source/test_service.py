058cc2860061fd7a5e979a1eb6e6cfba1cf38086
TAKOS2022 <justin.takodjou.1@ulaval.ca>
2024-12-08 13:07:29 -0500

    End of mocking with status codes error checking

--- tests/source/test_service.py
+++ tests/source/test_service.py
@@ -1,6 +1,7 @@
 import pytest
 import pytest_mastering.source.service as service
 import unittest.mock as mock
+import requests
 
 '''
 mock_get_user_fron_db arguments will be replace by the value of pytest_mastering.source.service.get_user_from_db
@@ -24,3 +25,14 @@ def test_get_users(mock_get):
     mock_get.return_value = mock_response
     data = service.get_users()
     assert data ==  {"id": 1, "name": "John Doe"}
+
+
+# This function test the status code of the request. The status should be different to 200. 
+# If status = 200 it will fail. 
+@mock.patch("requests.get")
+def test_get_users_error(mock_get):
+    mock_response = mock.Mock()
+    mock_response.status_code = 400
+    mock_get.return_value = mock_response
+    with pytest.raises(requests.HTTPError):
+        service.get_users()
