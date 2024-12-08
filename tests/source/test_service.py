83913d4ee3c322a5abe261e560e77613f48cc3b4
TAKOS2022 <justin.takodjou.1@ulaval.ca>
2024-12-08 13:00:52 -0500

    test for checking data fecth from api

--- tests/source/test_service.py
+++ tests/source/test_service.py
@@ -10,4 +10,17 @@ def test_get_user_from_db(mock_get_user_fron_db):
     # We modify the return value
     mock_get_user_fron_db.return_value = "Mocked Alice"
     user_name = service.get_user_from_db(1)
+
     assert user_name == "Mocked Alice"
+
+# We don't want to test the functions but the response
+@mock.patch("requests.get")
+def test_get_users(mock_get):
+    mock_response = mock.Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {
+        "id": 1, "name": "John Doe"
+    }
+    mock_get.return_value = mock_response
+    data = service.get_users()
+    assert data ==  {"id": 1, "name": "John Doe"}
