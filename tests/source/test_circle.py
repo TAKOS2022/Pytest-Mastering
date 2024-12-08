3cf2ed37929333b7b1c2c2d3975731a462ca7beb
TAKOS2022 <justin.takodjou.1@ulaval.ca>
2024-12-08 11:33:54 -0500

    End of class based test

--- tests/source/test_circle.py
+++ tests/source/test_circle.py
@@ -17,4 +17,9 @@ class TestCircle:
     def test_area(self):
         assert self.circle.area() == math.pi * self.circle.radius ** 2
 
+    def test_perimeter(self):
+        result = self.circle.perimeter()
+        expected = 2 *  math.pi * self.circle.radius
+        assert result == expected
+
   
